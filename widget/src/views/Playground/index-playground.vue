600ef0f5dd1ea0d7c062bed1945dc2a0fb98838e
Gabriel Fabiano <77023882+GabrielBitral@users.noreply.github.com>
2022-08-03 09:39:51 -0300

    Alteração final de uma frase

--- widget/src/views/Playground/index-playground.vue
+++ widget/src/views/Playground/index-playground.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-cian">
       <h1 class="text-6xl font-black text-brand-gray">Playground</h1>
-      <p class="text-2xl mt-3 font-regular text-brand-gray">Este é o playground, use para testar o widget.</p>
+      <p class="text-2xl mt-3 font-regular text-brand-gray">This is the playground, use it to test the widget.</p>
     </div>
     <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-gray">
       <h1 class="text-6xl font-black text-brand-graydark">🎡</h1>
